3c871e6501b552dc6d87715656d419f74ebaaf8e
{ Walyson } <walysonmoura222@gmail.com>
2024-04-24 12:25:08 +0000

    new

--- App.tsx
+++ App.tsx
@@ -3,7 +3,23 @@ import './src/translation';
 import 'react-native-gesture-handler';
 
 import RootStack from './src/navigation';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ThemeProvider } from 'styled-components/native';
+import { StatusBar } from 'react-native';
+import { Loading } from '@/components/Loading';
+
+import theme from '@/styles/theme';
 
 export default function App() {
-  return <RootStack />;
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="transparent" />
+
+        {fontsLoaded ? <RootStack /> : <Loading />}
+      </>
+    </ThemeProvider>
+  );
 }
--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,6 +1,9 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const Conatiner = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 
-
-`
+  background-color: ${({theme}) => theme};
+`;
